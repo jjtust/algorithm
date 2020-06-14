0d7e7c551cb69a6c0f209f9f31b72b2917a55c95
jjtust <66899051+jjtust@users.noreply.github.com>
2020-06-14 18:22:12 +0800

    Create insertSort.py
    插入排序

---
+++ insertSort.py
@@ -0,0 +1,19 @@
+class InsertSort:
+    def insertSort(arr):
+        n = len(arr)
+        for i in range(1, n):
+            # current value
+            tmp = arr[i]
+            # last value
+            j = i - 1
+            while j >= 0 and arr[j] > tmp:
+                # move the big last value to next
+                arr[j+1] = arr[j]
+                j -= 1
+            # put current value into the right position
+            arr[j+1] = tmp
+
+if __name__ == "__main__":
+    a = [3, 2, 1]
+    InsertSort.insertSort(a)
+    print(a)
