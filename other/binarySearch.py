8814472c0acd120ff51e19f3dbd4520a2807ed7e
jjtust <66899051+jjtust@users.noreply.github.com>
2020-07-05 21:35:34 +0800

    Update binarySearch.py

--- other/binarySearch.py
+++ other/binarySearch.py
@@ -1,14 +1,14 @@
 # O(logn)
-def binary_search(arr, num):
-    """二元搜索实现"""
+def binary_search(arr, target):
+    """二分搜索法实现"""
     l = 0
     r = len(arr) - 1
     while l <= r:
         mid = (l + r) // 2
-        if arr[mid] == num:
-            return num
-        elif arr[mid] < num:
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
             l = mid + 1
         else:
             r = mid - 1
-    return None
+    return -1
