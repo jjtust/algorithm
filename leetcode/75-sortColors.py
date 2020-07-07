6a6a4df6498f895bc5fd6f22d52e8a39dfc4a505
jjtust <66899051+jjtust@users.noreply.github.com>
2020-07-07 14:27:11 +0800

    Update 75-sortColors.py

--- leetcode/75-sortColors.py
+++ leetcode/75-sortColors.py
@@ -18,6 +18,30 @@ def sortColors(nums):
             nums[i] = color
         index += counts
 
+
+def sortColors(nums):
+    """使用三路快速排序算法进行排序"""
+    # 时间复杂度：O(n)
+    # 空间复杂度：O(1)
+    # 遍历一遍
+    zero = -1 # [0...zero] == 0
+    two = len(nums) # [two...n-1] == 2
+    i = 0 # [zero+1...two-1] == 1
+    
+    while i < two:
+        if nums[i] == 1:
+            i += 1
+        elif nums[i] == 0:
+            nums[zero+1], nums[i] = nums[i], nums[zero+1]
+            zero += 1
+            i += 1
+        else:
+            assert nums[i] == 2
+            nums[i], nums[two-1] = nums[two-1], nums[i]
+            two -= 1
+    
+        
+        
 nums = [2,0,2,1,1,0]
 sortColors(nums)
 print(nums)
