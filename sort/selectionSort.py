87a4bedaf32e6a92bd961d887e4df23a31e8f92a
jjtust <66899051+jjtust@users.noreply.github.com>
2020-07-11 17:41:43 +0800

    Update selectionSort.py

--- sort/selectionSort.py
+++ sort/selectionSort.py
@@ -1,13 +1,21 @@
 # O(n^2)
 def selection_sort(arr):
     """选择排序实现"""
+    
     size = len(arr)
+    
+    # 第一层循环[i...size)
     for i in range(size):
-        largest_index = i
+        lowerest_index = i
+        
+        # 第二层循环[i+1...size)
         for j in range(i+1, size):
-            if arr[j] < arr[largest_index]:
-                largest_index = j
-        arr[largest_index], arr[i] = arr[i], arr[largest_index]
+            # 寻找最小值
+            if arr[j] < arr[lowerest_index]:
+                lowerest_index = j
+                
+        # 不断将最小值移动到最左边
+        arr[lowerest_index], arr[i] = arr[i], arr[lowerest_index]
     return arr
 
 selection_sort([1, 3, 2])
